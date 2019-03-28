d502c6262d7840746d5c30ba53fbc57860325ab7
Wojciech Gomola <wojciech.gomola@grapeup.com>
2019-03-27 19:45:35 -0500

    Initial commit

---
+++ src/MediatR.AspNetCoreMvc.Helpers/ControllerExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MediatR.AspNetCoreMvc.Helpers
+{
+    public static class ControllerExtensions
+    {
+        public static async Task<IActionResult> Send<TRequest>(this IMediator mediator, TRequest request, CancellationToken token = default(CancellationToken)) where TRequest : IRequest
+        {
+            await mediator.Send(request, token);
+            return new OkResult();
+        }
+
+        public static async Task<IActionResult> Send<TRequest, TResult>(
+            this IMediator mediator, 
+            TRequest request,
+            CancellationToken token = default(CancellationToken)) where TRequest : IRequest<TResult>
+        {
+            var result = await mediator.Send(request, token);
+            return new OkObjectResult(result);
+        }
+    }
+}
---
+++ src/MediatR.AspNetCoreMvc.Helpers/MediatRController.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MediatR.AspNetCoreMvc.Helpers
+{
+    public class MediatRController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public MediatRController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public Task<IActionResult> Send<TRequest>(TRequest request, CancellationToken token = default(CancellationToken)) where TRequest : IRequest
+        {
+            return _mediator.Send<TRequest>(request, token);
+        }
+
+        public Task<IActionResult> Send<TRequest, TResult>(
+            TRequest request,
+            CancellationToken token = default(CancellationToken)) where TRequest : IRequest<TResult>
+        {
+            return _mediator.Send<TRequest, TResult>(request, token);
+        }
+    }
+}
