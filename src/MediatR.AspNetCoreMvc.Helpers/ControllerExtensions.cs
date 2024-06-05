abb0cad70e3e249d38fee3e0334bbb2b4db13e94
szogun1987 <szogun1987@gmail.com>
2024-06-05 15:56:58 +0200

    Update MediatR reference

--- src/MediatR.AspNetCoreMvc.Helpers/ControllerExtensions.cs
+++ src/MediatR.AspNetCoreMvc.Helpers/ControllerExtensions.cs
@@ -2,22 +2,21 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace MediatR.AspNetCoreMvc.Helpers
+namespace MediatR.AspNetCoreMvc.Helpers;
+
+public static class ControllerExtensions
 {
-    public static class ControllerExtensions
+    public static async Task<IActionResult> Send<TResponse>(
+        this Controller controller,
+        IMediator mediator, 
+        IRequest<TResponse> request,
+        CancellationToken token = default)
     {
-        public static async Task<IActionResult> Send<TResponse>(
-            this Controller controller,
-            IMediator mediator, 
-            IRequest<TResponse> request,
-            CancellationToken token = default(CancellationToken))
+        var result = await mediator.Send(request, token);
+        if (typeof(TResponse) == typeof(Unit))
         {
-            var result = await mediator.Send(request, token);
-            if (typeof(TResponse) == typeof(Unit))
-            {
-                return controller.Ok();
-            }
-            return controller.Json(result);
+            return controller.Ok();
         }
+        return controller.Json(result);
     }
 }
--- src/MediatR.AspNetCoreMvc.Helpers/MediatRController.cs
+++ src/MediatR.AspNetCoreMvc.Helpers/MediatRController.cs
@@ -2,22 +2,21 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace MediatR.AspNetCoreMvc.Helpers
-{
-    public class MediatRController : Controller
-    {
-        private readonly IMediator _mediator;
+namespace MediatR.AspNetCoreMvc.Helpers;
 
-        public MediatRController(IMediator mediator)
-        {
-            _mediator = mediator;
-        }
+public class MediatRController : Controller
+{
+    private readonly IMediator _mediator;
+
+    public MediatRController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
         
-        public Task<IActionResult> Send<TResponse>(
-            IRequest<TResponse> request,
-            CancellationToken token = default(CancellationToken))
-        {
-            return this.Send(_mediator, request, token);
-        }
+    public Task<IActionResult> Send<TResponse>(
+        IRequest<TResponse> request,
+        CancellationToken token = default)
+    {
+        return this.Send(_mediator, request, token);
     }
 }
